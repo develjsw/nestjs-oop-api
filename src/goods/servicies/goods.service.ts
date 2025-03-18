60dde9f355f8b2bd052822d390bb75d531a6d6f6
jsw <develjsw@gmail.com>
2025-03-18 23:40:09 +0900

    Interface 기반 주입 - 주석 추가

--- src/goods/servicies/goods.service.ts
+++ src/goods/servicies/goods.service.ts
@@ -4,6 +4,7 @@ import { CacheServiceInterface } from '../../cache/interfaces/cache-service.inte
 @Injectable()
 export class GoodsService {
     constructor(
+        // Interface 설정 값(ICacheService)과 실제 값(CacheServiceInterface)을 구분 하기위해 다르게 설정
         @Inject('ICacheService')
         private readonly cacheService: CacheServiceInterface
     ) {}
