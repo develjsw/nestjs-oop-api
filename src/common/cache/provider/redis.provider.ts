711ef321ab52acf2818043ed177c22fbac5b0924
jsw <develjsw@gmail.com>
2025-04-05 23:03:41 +0900

    redis provider 설정 - 유효성 검증 추가

--- src/common/cache/provider/redis.provider.ts
+++ src/common/cache/provider/redis.provider.ts
@@ -5,11 +5,36 @@ import { createClient, RedisClientType } from 'redis';
 @Injectable()
 export class RedisProvider implements OnModuleInit, OnModuleDestroy {
     private readonly redis: RedisClientType;
+    private readonly redisHost: string;
+    private readonly redisPort: number;
 
     constructor(private readonly configService: ConfigService) {
+        this.redisHost = this.loadRedisHost();
+        this.redisPort = this.loadRedisPort();
+
         this.redis = createClient({
-            url: `redis://${configService.get<string>('redis.host')}:${configService.get<number>('redis.port')}`
+            url: `redis://${this.redisHost}:${this.redisPort}`
         });
+    }
+
+    private loadRedisHost(): string {
+        const redisHost: string = this.configService.get<string>('redis.host');
+
+        if (!redisHost) {
+            throw new Error('RedisPost가 없습니다.');
+        }
+
+        return redisHost;
+    }
+
+    private loadRedisPort(): number {
+        const redisPort: number = this.configService.get<number>('redis.port');
+
+        if (!redisPort) {
+            throw new Error('RedisPort가 없습니다.');
+        }
+
+        return redisPort;
     }
 
     async onModuleInit() {
